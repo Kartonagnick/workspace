bcfc7d9b1011e0cec5f673c2fbee9ffd312d658a
Kartonagnick <CastleOfDreams@yandex.ru>
2021-01-08 07:03:06 +0300

    add: cmdhello

---
+++ projects/cmdhello/src/pch.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#ifdef _MSC_VER
+    #ifdef STABLE_RELEASE
+        #pragma message("Build stable release version")
+    #else
+        #pragma message("Build development version")
+    #endif
+#endif
+
+#include <iostream>
+
+
